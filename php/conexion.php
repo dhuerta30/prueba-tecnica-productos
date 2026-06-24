ecc5271d56cf628efa3fbe490a8c08fa0f7d5cd0
dhuerta30 <63071903+dhuerta30@users.noreply.github.com>
2026-06-24 10:14:07 -0400

    up

--- php/conexion.php
+++ php/conexion.php
@@ -14,7 +14,7 @@ try {
             PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
             // CLAVE contra inyección sql
             PDO::ATTR_EMULATE_PREPARES => false,
-            // asegura fetch más seguro por defecto (opcional pero recomendado)
+            // asegura fetch más seguro por defecto
             PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
             // evita múltiples queries en un prepare
             PDO::MYSQL_ATTR_MULTI_STATEMENTS => false
